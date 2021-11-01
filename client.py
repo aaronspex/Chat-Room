78c1ecad84738333969c3389ae42ad3c27d4ce1c
aaronspex <aaronspex@gmail.com>
2021-11-01 00:57:52 -0400

    Trying to get chat function to work, its not there yet

--- client.py
+++ client.py
@@ -4,25 +4,33 @@ import threading
 host = "127.0.0.1"
 port = 1337
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-
-
+seshID = 0
 
 print("PLEASE ENTER IN A NICKNAME IN ORDER TO JOIN THE CHATROOM!\n")
 nickname = input("Enter Nickname Here: ")
 s.sendto(f"[com,JOIN,{nickname},{host}".encode('utf-8'), (host,port))
+data, addr = s.recvfrom(1024)
+seshID = data
+
 
 
 def send():        
     while True:
         msg = input('Message to send: ')
-        s.sendto(msg.encode('utf-8'), (host, port))
+        print("DOSing Server")
+        parseChat(msg)
 
 
 def receive():
     while True:
         data, addr = s.recvfrom(1024)
         print(data)
+
+def parseChat(msg):
+    chat = f"[cha,{nickname},{msg},]"
+    s.sendto(chat.encode('utf-8'), (host, port))
+    
+    
 
 t1 = threading.Thread(target = send, args=())
 t1.start()
--- server.py
+++ server.py
@@ -14,25 +14,37 @@ nicknames = []
 print(f"Waiting on port: {PORT}")
 
 def addUser(data, addr):
-    clients.append(addr)
+    seshID = random.randint(1000,9998)
+    s.sendto(seshID, (HOST, PORT))
+    clients.append((addr, seshID))
     nicknames.append(data)
+
     print(f"NEW USER: {data}, {addr}")
+    print(clients)
+
+def broadcast(sender, message):
+    chat = f"[cha,{sender},{message},]"
+    for client in clients:
+        s.sendto(chat.encode('utf-8'), (client[0], PORT))
+
 
 
 def parseChat(rawMsg, addr):
     msg = rawMsg.decode('utf-8')
+    splitMsg = msg.split(",")
     if msg[1:4] == "com":
         if msg[5:9] == "JOIN":
-            addUser(msg.split(",")[2], msg.split(",")[3])
+            addUser(splitMsg[2], splitMsg[3])
     else:
         print(f"CHAT RECIEVED: {msg}")
+        broadcast(splitMsg[1], splitMsg[2])
 
 
 
 
 
-#{com,JOIN,NICKNAME,HOSTNAME}
-#{cha:NICKNAME CHAT-CONTENTS}
+#[com,JOIN,NICKNAME,HOSTNAME,]
+#[cha,NICKNAME CHAT-CONTENTS,]
 
 while True:
     data, addr = s.recvfrom(1024)
